f825f32cc15e5d10bb41aca49ccb52fed647c764
default <default>
2019-05-17 10:55:31 +0000

    Adding initial code structure

---
+++ src/main/java/com/infy/jacoco/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.infy.jacoco.config.audit;
---
+++ src/main/java/com/infy/jacoco/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.infy.jacoco.web.rest.vm;
